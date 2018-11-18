8b608ef5a150a616f5c9ff93eb9d7fae1e9305c5
nonodepinfo <40430836+nonodepinfo@users.noreply.github.com>
2018-11-18 20:24:58 +0100

    Modifications d'affichages pour faire comme find

--- listing.c
+++ listing.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
@@ -15,22 +14,37 @@ int printfBis(char* nom);
 void main(int argc, char *argv[]){
 
     if (argc<2){
-        printf("missing argument");
+        printf("missing argument\n");
         exit(1);
         //cas où on n'a pas d'argument
     }
 
 
   DIR *dirp=opendir(argv[1]); // il faut mettre le chemin si c'est un dossier antérieur, le nom suffit si c'est un descendant
+
+  if (dirp==NULL){
+        printf("rsfind: %s: No such file or directory\n",argv[1]); //doit renvoyer rsfind: ou find:  ???
+        exit(2);
+  }
+
   struct dirent *dp;
   char *table[1000];
   int i=0;
 
   while ((dp=readdir(dirp))!=NULL){
-    table[i]=dp->d_name;
-    i+=1;
+    if (strcmp(dp->d_name,".") && strcmp(dp->d_name,"..")){
+        table[i]=dp->d_name;
+        i+=1;
+    }
+
   }
   closedir(dirp);
+
+  if (i==0){ //le directory est vide
+    printf("%s\n",argv[1]);
+    exit(2);
+  }
+
 
   qsort(&table[0],i,sizeof(char*),compare);
 
